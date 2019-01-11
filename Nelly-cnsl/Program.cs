1e6e81f222e9004da6b25f6ddb9eb8d00cfebd8e
Ivan <ivankoy@gmail.com>
2019-01-11 20:11:33 +0100

    Add project files.

---
+++ Nelly-cnsl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nelly_cnsl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
