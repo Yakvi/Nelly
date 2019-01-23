b0277631f5c3ae205d81225db9bc28c87535be5c
Ivan <ivankoy@gmail.com>
2019-01-23 07:38:45 +0100

    Added an ending and linked open branches to it

--- Nelly.Database/UnitRepository.cs
+++ Nelly.Database/UnitRepository.cs
@@ -14,33 +14,39 @@ namespace Nelly.Database
 
             var u0 = AddUnit("data/cutscene1.txt"); // Cutscene and first quest
             var u1 = AddUnit("data/choice1.txt"); // How do you want to get there? 0. Bus 1. Tram
-            var u2 = AddBranch("data/walking-meet.txt", u1, 0); // Bus 
+            var u2 = AddUnit("data/walking-meet.txt", u1, 0); // Bus 
             var u3 = AddUnit("data/choice2.txt"); // Give money? 0. Yes 1. No
-            var u4 = AddBranch("data/give-money.txt", u3, 0); // Yes
-            var u5 = AddBranch("data/no-money.txt", u3, 1); // No
-            var u6 = AddBranch("data/tram-way.txt", u1, 1); // Tram
-            var u7 = AddUnit("data/choice3.txt"); // Get off now, continue riding
+            var u4 = AddUnit("data/give-money.txt", u3, 0); // Yes, finish tree
+            var u5 = AddUnit("data/no-money.txt", u3, 1); // No, finish tree
+            var u6 = AddUnit("data/tram-way.txt", u1, 1); // Tram
+            var u7 = AddUnit("data/choice3.txt"); // 1. Continue further, 2. Get off later, 3. Get off now
+
+            var end = AddUnit("data/end.txt");
+            u4.AddNextUnitId(end.Id, 0);
+            u5.AddNextUnitId(end.Id, 0);
 
             return units[0];
         }
 
-        private static Unit AddUnit(string fileName)
-        {
-            var unit = new Unit(fileName, guid++);
-            units.Add(unit);
-
-            return unit;
-        }
-
-        private static Unit AddBranch(string fileName, Unit parent, int index)
+        private static Unit AddUnit(string fileName, Unit parent, int index)
         {
             var result = new Unit(fileName, guid);
             units.Add(result);
-            parent.AddNextUnitId(guid, index);
+            if (parent != null)
+            {
+                parent.AddNextUnitId(result.Id, index);
+            }
 
             ++guid;
 
             return result;
+        }
+
+        private static Unit AddUnit(string fileName)
+        {
+            var unit = AddUnit(fileName, null, 0);
+
+            return unit;
         }
 
         internal static Unit Get(int index)
