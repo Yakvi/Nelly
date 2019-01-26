742496df518bcb2bb4fee26115a4965bd69f00d8
Ivan <ivankoy@gmail.com>
2019-01-26 15:34:35 +0100

    Added unity project

---
+++ Nelly.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public Slide firstSlide;
+    private bool isPaused = false;
+    public GameObject mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        mainMenu.SetActive(false);
+    }
+}
